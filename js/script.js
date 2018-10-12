2d3bf838a58d8239e6ac3267593a2cf317856ec0
JKozma81 <janos.kozma81@gmail.com>
2018-10-12 05:09:19 +0200

    Working

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+/*
+<i class="far fa-check-circle"></i>  OK
+<i class="fas fa-exclamation-triangle"></i> Error
+
+border-bottom: 3px solid #1e06f8; input field border style on focus
+border-bottom: 3px solid #4bf806; input field border style on ok
+border: 3px solid #f80606; input field border style on error
+
+*/
+
+// DOM Ready event handler
 $(() => {
 // Selecting form elements for further work
 const $userNameField = $('#name');
@@ -10,6 +21,7 @@ const $shirtdesignContainer = $('.shirt div').eq(1);
 const $shirtDesign = $('#design');
 const $shirtColorContainer = $('#colors-js-puns')
 const $shirtColor = $('#color');
+const $colors = $('#color option');
 
 const $activities = $('.activities');
 
@@ -21,16 +33,42 @@ const $cvv = $('#cvv');
 const $expMonth = $('#exp-month');
 const $expYear = $('#exp-year');
 
-
-
-
 const $submit = $('button');
+
+
+// Listening to change event and if the other option is chosen then the textfield shows to enter job role
+$jobTitle.on('change', () => {
+   if ($jobTitle.val() === 'other') {
+    $jobRoleField.show();
+   } else {
+    $jobRoleField.hide();
+   }
+});
+
+$shirtDesign.on('change', () => {
+    $shirtColorContainer.show();
+
+});
 
 
 
 // On DOM Ready event the focus is set to the input for the User name
 $userNameField.focus();
 $jobRoleField.hide();
+$shirtColorContainer.hide();
+
+if ($shirtDesign.val() === 'Select Theme') {
+
+    $colors.eq(0).text('Please select a T-shirt theme');
+} else if ($shirtDesign.val() === 'js puns') {
+    $colors.eq(0).show();
+    $colors.eq(1).show();
+    $colors.eq(2).show();
+} else {
+    $colors.eq(3).show();
+    $colors.eq(4).show();
+    $colors.eq(5).show();
+}
 
 
 
@@ -38,5 +76,6 @@ $jobRoleField.hide();
 $submit.on('click', (e) => {
     e.preventDefault();
 })
+
 })
 
